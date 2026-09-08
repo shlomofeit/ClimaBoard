eae258a744442a08c9ed98fd99543692cbc8a713
shlomofeit <11.5sfeit@gmail.com>
2026-09-08 17:16:40 +0300

    Fix from max_length to min_length in the cities router

--- backend/app/routers/cities_router.py
+++ backend/app/routers/cities_router.py
@@ -7,7 +7,7 @@ router = APIRouter(tags=["Cities"])
 @router.get("/search")
 def get_cities(
     city_search: str = Query(
-        ..., max_length=2, description="returns cities array by your string"
+        ..., min_length=2, description="returns cities array by your string"
     )
 ):
     cities_list = get_coordinates(city_search)
