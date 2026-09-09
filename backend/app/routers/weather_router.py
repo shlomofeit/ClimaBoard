49bb636c33259f664bf7dcc901240661a23bfa99
shlomofeit <11.5sfeit@gmail.com>
2026-09-09 11:21:45 +0300

    Added compare cities router

--- backend/app/routers/weather_router.py
+++ backend/app/routers/weather_router.py
@@ -5,7 +5,7 @@ router = APIRouter(tags=["Weather"])
 
 
 @router.get("/current")
-def get_current(lat: float, lon: float):
+def get_current(lat, lon):
     current = get_current_weather(lat, lon)
 
     if not current:
@@ -15,10 +15,23 @@ def get_current(lat: float, lon: float):
 
 
 @router.get("/forecast")
-def get_current(lat: float, lon: float):
+def get_current(lat, lon):
     forecast = get_forecast_weather(lat, lon)
 
     if not forecast:
         raise HTTPException(404, "wether no found")
 
     return forecast
+
+
+@router.get("/compare")
+def compare_cities(
+    lat1: float = Query(..., ge=-90.0, le=90.0),
+    lon1: float = Query(..., ge=-180.0, le=180.0),
+    lat2: float = Query(..., ge=-90.0, le=90.0),
+    lon2: float = Query(..., ge=-180.0, le=180.0),
+):
+    current1 = get_current_weather(lat1, lat1)
+    current2 = get_current_weather(lat2, lat2)
+
+    return {"current_1": current1, "current_2": current2}
