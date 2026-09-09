4930d19a7ce21f4c6ce59720b2fdb077ac941ac6
shlomofeit <11.5sfeit@gmail.com>
2026-09-09 11:21:07 +0300

    Fix logger format

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s" | "%(levelname)s" | "%(message)s",
+    format="%(asctime)s | %(levelname)s | %(message)s",
     handlers=[logging.FileHandler("logs/log.app"), logging.StreamHandler()],
 )
 
